bba78030c935d30b46263819dbcc7fed41b51cd6
Karl Engström <kalle.engstrom@gmail.com>
2017-02-09 22:40:48 +0100

    seeding rand

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"github.com/kengstrom/gorka/gorka"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	fmt.Println("Starting game...")
 	var game gorka.Game
 	game.CreateGame(1,3)
